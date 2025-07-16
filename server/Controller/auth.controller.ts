b003171ac919336f8581874914ec7cb40c9ebe2e
islam-heddi <islamheddi19@gmail.com>
2025-07-16 17:26:19 +0100

    server: added singin

--- server/Controller/auth.controller.ts
+++ server/Controller/auth.controller.ts
@@ -1,14 +1,39 @@
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 import { UserValidation } from "../utils/validations";
 import type { Request, Response } from "express";
 import { User } from "../Model/Users.model";
+import jwt from "jsonwebtoken";
 
-const signIn = async (req: Request, res: Response) => {
+export const signIn = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
   try {
-  } catch (error) {}
+    const result = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!result) return res.status(400).send("email not found");
+    const check = await compare(password, result.password as string);
+    if (!check) return res.status(400).send("password does not match");
+    const token = jwt.sign(
+      { userId: result._id },
+      process.env.SECRET_KEY as string,
+      {
+        expiresIn: 15 * 24 * 60 * 60 * 100,
+      }
+    );
+    res.cookie("token", token, {
+      maxAge: 15 * 24 * 60 * 60 * 100,
+      secure: true,
+    });
+
+    return res.status(201).send("successfully logged in");
+  } catch (error) {
+    return res.status(500).send(`error: ${error}`);
+  }
 };
 
-const signUp = async (req: Request, res: Response) => {
+export const signUp = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
   try {
     const data = UserValidation.parse({ email, password, name });
