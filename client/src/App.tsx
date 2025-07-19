75c8937bbaff1ebebf25a4c3422a56175c3c760d
islam-heddi <islamheddi19@gmail.com>
2025-07-19 13:16:44 +0100

    client: create socket context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dasboard from "./pages/Dashboard";
 import ChatPage from "./pages/ChatPage";
+import { SocketProvider } from "./context/SocketContext";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dasboard />} />
-        <Route path="/chat/:friendId" element={<ChatPage />} />
-      </Routes>
-    </BrowserRouter>
+    <SocketProvider>
+      {/* Wrapping the application with BrowserRouter to enable routing */}
+      {/* The Routes component defines the different routes in the application */}
+      {/* Each Route component specifies a path and the component to render for that path */}
+      {/* Home component is rendered at the root path */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dasboard />} />
+          <Route path="/chat/:friendId" element={<ChatPage />} />
+        </Routes>
+      </BrowserRouter>
+      {/* Dasboard component is rendered at the /dashboard path */}
+    </SocketProvider>
   );
 }
 
--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -1,4 +1,48 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useRef } from "react";
+import { useAppStore } from "@/store";
+import { HOST } from "@/utils/constants";
+import { io } from "socket.io-client";
+import { Socket } from "socket.io-client";
+import type { DefaultEventsMap } from "@socket.io/component-emitter";
 
-const SocketContext = createContext(null);
+const SocketContext = createContext<Socket<
+  DefaultEventsMap,
+  DefaultEventsMap
+> | null>(null);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useSocket = () => {
+  useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const { userInfo } = useAppStore();
+  const socket = useRef<Socket<DefaultEventsMap, DefaultEventsMap>>(null);
+
+  useEffect(() => {
+    if (userInfo) {
+      socket.current = io(HOST, {
+        withCredentials: true,
+        query: {
+          userId: userInfo._id,
+        },
+      }) as Socket<DefaultEventsMap, DefaultEventsMap>;
+      socket.current.on("connect", () => {
+        console.log("Socket connected:", socket.current?.id);
+      });
+    }
+
+    return () => {
+      socket.current?.disconnect();
+      console.log("Socket disconnected");
+    };
+  }, [userInfo]);
+
+  return (
+    <SocketContext.Provider value={socket.current}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
 export default SocketContext;
